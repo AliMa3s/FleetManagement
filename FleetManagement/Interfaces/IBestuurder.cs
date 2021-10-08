4c028df247095bca6953f5b9da5f6d7971de5e58
Filip Rigoir <filip.rigoir@telenet.be>
2021-10-08 17:52:28 +0200

    Map & Interfaces toegevoegd

---
+++ FleetManagement/Interfaces/IBestuurder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FleetManagement.Interfaces
+{
+    interface IBestuurder
+    {
+        public void BestuurderLoskoppelenVanVoertuig();
+        public void BestuurderAanVoertuigKoppelen();
+    }
+}
