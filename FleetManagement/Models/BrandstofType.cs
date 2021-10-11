26d69aefe1b1ad2d9c553a0a2d9a1b5163d15856
Filip Rigoir <filip.rigoir@telenet.be>
2021-10-11 10:38:24 +0200

    BrandstofType uitgebreid met brandstofAfkorting. Kan later ook nog uitbreiden: normen, etikettering, beschrijving...

--- FleetManagement/Models/BrandstofType.cs
+++ FleetManagement/Models/BrandstofType.cs
@@ -5,23 +5,26 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace FleetManagement.Models {
-    public class BrandstofType {
-        public int BrandstofTypeId { get; private set; }  //Id mag nooit veranderen. get alleen zou ik overwegen
-        public string BrandstofNaam { get; private set; } //brandstofnaam moet de klant kunnen wijzigen (Diesel/Hybride naar Hybride-diesel bijvoorbeeld, dat mag de klant zelf bepalen)
 
-        public BrandstofType(int brandstofTypeId, string brandstofNaam) { //Als je instantie maakt van waar komt het brandstofTypeId?
-            BrandstofTypeId = brandstofTypeId;
+    public class BrandstofType {
+
+        public int BrandstofTypeId { get; } 
+
+        public string BrandstofNaam { get; } 
+
+        public string BrandstofAfkorting { get; }
+
+        //Uitbreiding mogelijk: etikettering, normen, beschrijving, enz.
+
+        public BrandstofType(string brandstofNaam, string brandstofAfkorting) { 
             BrandstofNaam = brandstofNaam;
+            BrandstofAfkorting = brandstofAfkorting;
+        }
+
+        public BrandstofType(int brandstofTypeId, string brandstofNaam, string brandstofAfkorting)
+            : this(brandstofNaam, brandstofAfkorting)
+        {
+            BrandstofTypeId = brandstofTypeId;
         }
     }
 }
-
-/* 
- * Er is een beslingskeuze hier:
- * Brandstoftype kan wijzigen. Zo kan bijvoorbeeld diesel na 5 jaar verboden worden
- * Of kan een andere Brandstof de wereld veroveren
- * De klant moet dat in de hand hebben en kunnen aanmaken zonder programmeur
- * Maar kan ook uiteraard enum zijn (dan kan de klant het vragen, dat gebeurt waarschijnlijk niet zoveel)
- * Als het enum is kan de Id in constructor. Maar dan moet je wel de vraag stellen. Wie controleerd dat best? Presentatielaag (id in ctor; id van buitenaf) of businesslaag (id checken in logica aan de hand van de naam in enum/db).
- * Beslis wijs en implementeer één van die twee aub! De keuze is aan de maker van deze class. 
-*/
