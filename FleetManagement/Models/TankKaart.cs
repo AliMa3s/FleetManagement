a305e8c6548c3b2fadfbbe5c84cc812e887f1a3b
alimaes <ali.maes@student.hogent.be>
2021-10-12 06:05:24 +0200

    ctor error opgelost

--- FleetManagement/Models/TankKaart.cs
+++ FleetManagement/Models/TankKaart.cs
@@ -10,24 +10,24 @@ namespace FleetManagement.Models {
         //Zone Properties
         public int TankKaartId { get; private set; }
         public string KaartNummer { get; private set; }
-        public DateTime VervalDatum { get; set; } 
+        public DateTime VervalDatum { get; set; }
         public DateTime UitgeefDatum { get; set; }
         public string Pincode { get; private set; } = string.Empty;
         public List<string> BrandstofType { get; private set; }
         public Bestuurder? Bestuurder { get; set; } = null;
 
         //Ctor 
-        public TankKaart() { 
+        public TankKaart() {
         }
-        public TankKaart(int tankKaartId, string kaartNummer, DateTime vervalDatum, string pincode, DateTime uitgeefdatum/*, Bestuurder bestuurder*/) {
-            TankKaartId = tankKaartId; 
-            KaartNummer = kaartNummer;  
+        public TankKaart(int tankKaartId, string kaartNummer, DateTime vervalDatum, string pincode, DateTime uitgeefdatum, Bestuurder bestuurder) {
+            TankKaartId = tankKaartId;
+            KaartNummer = kaartNummer;
             VervalDatum = vervalDatum;
             UitgeefDatum = uitgeefdatum;
-            Pincode = pincode; 
-            BrandstofType = brandstofType;
-            //this.bestuurder = bestuurder;
-            //
+            Pincode = pincode;
+            //BrandstofType = brandstofType;
+            Bestuurder = bestuurder;
+
 
             BrandstofType = new List<string>();
         }
@@ -58,17 +58,17 @@ namespace FleetManagement.Models {
         }
 
         public void VoegKaartNummerToe(string kaartnummer) {
-            if (!string.IsNullOrWhiteSpace(kaartnummer)) { 
+            if (!string.IsNullOrWhiteSpace(kaartnummer)) {
                 KaartNummer = kaartnummer;
             } else {
-                throw new TankKaartException("Kaart nummer kan niet leeg zijn"); 
+                throw new TankKaartException("Kaart nummer kan niet leeg zijn");
             }
         }
 
         public void VoegPincodeToe(string pincode) {
             if (!string.IsNullOrWhiteSpace(pincode)) {
                 if (pincode.Length < 4) {
-                    throw new TankKaartException("Pincode moet 4 karakter zijn"); 
+                    throw new TankKaartException("Pincode moet 4 karakter zijn");
                 } else {
                     Pincode = pincode;
                 }
