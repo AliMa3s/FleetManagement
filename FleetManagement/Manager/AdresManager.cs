49f5393bf7d24a954bfa0eb1906bb92f8a9a13c5
alimaes <ali.maes@student.hogent.be>
2021-10-28 15:23:12 +0200

    AdresManager-MethodesImplementeert

--- FleetManagement/Manager/AdresManager.cs
+++ FleetManagement/Manager/AdresManager.cs
@@ -1,4 +1,5 @@
 ﻿using FleetManagement.Interfaces;
+using FleetManagement.ManagerExceptions;
 using FleetManagement.Model;
 using System;
 using System.Collections.Generic;
@@ -8,24 +9,78 @@ using System.Threading.Tasks;
 
 namespace FleetManagement.Manager {
     public class AdresManager : IAdresRepository {
+        private IAdresRepository repo;
+
+        public AdresManager(IAdresRepository repo) {
+            this.repo = repo;
+        }
+
         public bool BestaatAdres(Adres adres) {
-            throw new NotImplementedException();
+            try {
+                if (adres == null) throw new AdresManagerException("Adres mag niet null zijn");
+                if (!repo.BestaatAdres(adres)) {
+                    return false;
+                } else {
+                    return true;
+                }
+            } catch (Exception ex) {
+                throw new AdresManagerException("Adres - BestaatAdres - Foutief", ex);
+            }
         }
 
         public bool BestaatAdres(int adresId) {
-            throw new NotImplementedException();
+            try {
+                if (adresId < 1) throw new AdresManagerException("Adres kan niet kleiner dan 0 zijn");
+                if (!repo.BestaatAdres(adresId)) {
+                    return false;
+                } else {
+                    return true;
+                }
+            } catch (Exception ex) {
+                throw new AdresManagerException("Adres - BestaatAdres - Foutief", ex);
+            }
         }
 
         public void UpdateAdres(Adres adres) {
-            throw new NotImplementedException();
+            try {
+                if (adres == null) throw new AdresManagerException("Adres - Adres mag niet null zijn");
+                if (repo.BestaatAdres(adres)) {
+                    repo.UpdateAdres(adres);
+                } else {
+                    throw new AdresManagerException("Adres - bestaat niet!");
+                }
+            } catch (Exception ex) {
+
+                throw;
+            }
         }
 
         public void VerwijderAders(Adres adres) {
-            throw new NotImplementedException();
+            try {
+                if (adres == null) throw new AdresManagerException("Adres - Adres mag niet null zijn");
+                if (repo.BestaatAdres(adres)) {
+                    repo.VerwijderAders(adres);
+                } else {
+                    throw new AdresManagerException("Adres - bestaat niet!");
+                }
+            } catch (Exception ex) {
+
+                throw new AdresManagerException(ex.Message);
+            }
         }
 
         public void VoegAdresToe(Adres adres) {
-            throw new NotImplementedException();
+            try {
+                if (adres == null) throw new AdresManagerException("Adres - Adres mag niet null zijn");
+                if (!repo.BestaatAdres(adres.AdresId)) {
+                    repo.VoegAdresToe(adres);
+                } else {
+                    throw new AdresManagerException("Adres Bestaat al");
+                }
+            } catch (Exception ex) {
+
+                throw new AdresManagerException(ex.Message);
+            }
         }
     }
 }
