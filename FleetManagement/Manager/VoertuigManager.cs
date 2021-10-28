c81f80375bb6182ba181200ebd887dee3c634e73
alimaes <ali.maes@student.hogent.be>
2021-10-28 14:28:00 +0200

    interface,manager-structuur

---
+++ FleetManagement/Manager/VoertuigManager.cs
@@ -0,0 +1,11 @@
+﻿using FleetManagement.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FleetManagement.Manager {
+    public class VoertuigManager : IVoertuigRepository {
+    }
+}
