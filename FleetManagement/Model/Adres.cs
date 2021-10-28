2b9e57e47c3fa17a976f2f99f45c19cb1645c2be
alimaes <ali.maes@student.hogent.be>
2021-10-28 18:07:05 +0200

    Adres - stad veranderd naar gemeente + adressql file

--- FleetManagement/Model/Adres.cs
+++ FleetManagement/Model/Adres.cs
@@ -5,41 +5,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FleetManagement.Model
-{
-    public class Adres
-    {
+namespace FleetManagement.Model {
+    public class Adres {
         public int AdresId { get; }
         public string Straat { get; set; }
         public string Nr { get; set; }
         public string Postcode { get; set; }
-        public string Stad { get; set; }
+        public string Gemeente { get; set; }
 
-        public Adres(string straat, string nr, string postcode, string stad)
-        {
+        public Adres(string straat, string nr, string postcode, string stad) {
             Straat = straat;
             Nr = nr;
             Postcode = postcode;
-            Stad = stad;
+            Gemeente = stad;
         }
 
         public Adres(int adresId, string straat, string nr, string postcode, string stad)
-            : this(straat, nr, postcode, stad)
-        {
-            if (adresId > 0)
-            {
+            : this(straat, nr, postcode, stad) {
+            if (adresId > 0) {
                 AdresId = adresId;
-            }
-            else
-            {
+            } else {
                 throw new AdresException($"{nameof(AdresId)} moet meer zijn dan 0");
             }
         }
 
-        public override string ToString()
-        { 
+        public override string ToString() {
             StringBuilder build = new($"{Straat} {Nr}");
-            build.Append("{Postcode} {Stad}");
+            build.Append("{Postcode} {Gemeente}");
             return build.ToString();
         }
     }
