be7b30ac7c92d6f2c5b6fe4f002b5bc4b3045374
alimaes <ali.maes@student.hogent.be>
2021-10-29 18:47:16 +0200

    brandstof + tankkaart - SQL

---
+++ FleetManagement.ADO/SQL/Brandstoftype.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Brandstoftype] (
+    [brandstofid]   INT           NOT NULL,
+    [brandstofnaam] NVARCHAR (55) NOT NULL,
+    PRIMARY KEY CLUSTERED ([brandstofid] ASC)
+);
---
+++ FleetManagement.ADO/SQL/tankkaart_brandstoftype.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Tankkaart_Brandstoftype] (
+    [tankkaartnummer] NVARCHAR (19) NOT NULL,
+    [brandstoftypeid] INT           NOT NULL,
+    CONSTRAINT [PK_Tankkaart_Brandstoftype] PRIMARY KEY CLUSTERED ([tankkaartnummer] ASC),
+    CONSTRAINT [FK_Tankkaart_Brandstoftype_tankkaart] FOREIGN KEY ([tankkaartnummer]) REFERENCES [dbo].[Tankkaart] ([kaartnummer]),
+    CONSTRAINT [FK_Tankkaart_Brandstoftype_brandstoftype] FOREIGN KEY ([brandstoftypeid]) REFERENCES [dbo].[Brandstoftype] ([brandstofid])
+);
