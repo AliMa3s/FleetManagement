7a60db3473cfdabfe11e2d88238a4abf626e8e8f
alimaes <ali.maes@student.hogent.be>
2021-10-29 18:52:25 +0200

    automodel- kleursql

---
+++ FleetManagement.ADO/SQL/AutoModel.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Automodel] (
+    [automodelid]    INT            NOT NULL,
+    [merk_naam]      NVARCHAR (55)  NOT NULL,
+    [automodel_naam] NVARCHAR (100) NOT NULL,
+    [autotype]       NVARCHAR (55)  NOT NULL,
+    PRIMARY KEY CLUSTERED ([automodelid] ASC)
+);
---
+++ FleetManagement.ADO/SQL/Kleur.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Kleur] (
+    [kleurid]   INT           NOT NULL,
+    [kleurnaam] NVARCHAR (55) NOT NULL,
+    PRIMARY KEY CLUSTERED ([kleurid] ASC)
+);
