276f3d30c115abfe57fb73c49588ffcd30311ae2
alimaes <ali.maes@student.hogent.be>
2021-10-29 18:25:06 +0200

    BestuurderSQL

--- FleetManagement.ADO/SQL/Adres.sql
+++ FleetManagement.ADO/SQL/Adres.sql
@@ -1,1 +1,8 @@
-﻿
+﻿CREATE TABLE [dbo].[Adres] (
+    [adresid]  INT          IDENTITY (1, 1) NOT NULL,
+    [straat]   VARCHAR (55) NULL,
+    [nummer]   NCHAR (10)   NULL,
+    [gemeente] NCHAR (55)   NULL,
+    [postcode] NCHAR (55)   NULL,
+    PRIMARY KEY CLUSTERED ([adresid] ASC)
+);
---
+++ FleetManagement.ADO/SQL/Bestuurder.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Bestuurder] (
+    [bestuurderid]        INT            IDENTITY (1, 1) NOT NULL,
+    [adresid]             INT            NULL,
+    [voornaam]            NVARCHAR (50)  NOT NULL,
+    [achternaam]          NVARCHAR (50)  NOT NULL,
+    [geboortedatum]       DATE           NOT NULL,
+    [rijksregisternummer] NVARCHAR (11)  NOT NULL,
+    [rijbewijstype]       NVARCHAR (100) NOT NULL,
+    [rijbewijsnummer]     NVARCHAR (10)  NOT NULL,
+    [aanmaakDatum]        ROWVERSION     NOT NULL,
+    PRIMARY KEY CLUSTERED ([bestuurderid] ASC),
+    CONSTRAINT [FK_Table_Adres] FOREIGN KEY ([adresid]) REFERENCES [dbo].[Adres] ([adresid])
+);
