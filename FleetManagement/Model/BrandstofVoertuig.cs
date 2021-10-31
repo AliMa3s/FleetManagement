5cea44a3581cf5f4c5cf7e9f698ad518ed134845
Filip Rigoir <filip.rigoir@telenet.be>
2021-10-31 22:45:24 +0100

    Overleg met Amhet, overbodige prop verwijderd

--- FleetManagement/Model/BrandstofVoertuig.cs
+++ FleetManagement/Model/BrandstofVoertuig.cs
@@ -9,8 +9,6 @@ namespace FleetManagement.Model
     public class BrandstofVoertuig : BrandstofType
     {
         public bool Hybride { get; set; }
-
-        public string aandrijving => Hybride ? "Hybride " + BrandstofNaam : BrandstofNaam;
         
         public BrandstofVoertuig(string brandstofNaam, bool isHybride) : base(brandstofNaam)
         {
