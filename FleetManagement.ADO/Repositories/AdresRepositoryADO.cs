7120cfc529e422eb447e435df910636da46a0181
alimaes <ali.maes@student.hogent.be>
2021-11-01 05:27:01 +0100

    Bestaatadres-ado-commit

--- FleetManagement.ADO/Repositories/AdresRepositoryADO.cs
+++ FleetManagement.ADO/Repositories/AdresRepositoryADO.cs
@@ -1,8 +1,10 @@
-﻿using FleetManagement.Interfaces;
+﻿using FleetManagement.ADO.RepositoryExceptions;
+using FleetManagement.Interfaces;
 using FleetManagement.Model;
 using Microsoft.Data.SqlClient;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,11 +23,51 @@ namespace FleetManagement.ADO.Repositories {
         }
 
         public bool BestaatAdres(Adres adres) {
-            throw new NotImplementedException();
+            SqlConnection connection = getConnection();
+            string query = "SELECT count(*) FROM Adres WHERE straat=@straat AND nummer=@nummer AND postcode=@postcode AND gemeente=@gemeente";
+            using (SqlCommand command = connection.CreateCommand()) {
+                connection.Open();
+                try {
+                    command.Parameters.Add(new SqlParameter("@straat", SqlDbType.NVarChar));
+                    command.Parameters.Add(new SqlParameter("@nummer", SqlDbType.NVarChar));
+                    command.Parameters.Add(new SqlParameter("@postalCode", SqlDbType.NVarChar));
+                    command.Parameters.Add(new SqlParameter("@gemeente", SqlDbType.NVarChar));
+
+                    command.Parameters["@straat"].Value = adres.Straat;
+                    command.Parameters["@nummer"].Value = adres.Nr;
+                    command.Parameters["@postcode"].Value = adres.Postcode;
+                    command.Parameters["@gemeente"].Value = adres.Gemeente;
+
+                    command.CommandText = query;
+                    int n = (int)command.ExecuteScalar();
+                    if (n > 0) return true; else return false;
+                } catch (Exception ex) {
+                    throw new AdresRepositoryADOException("BestaatAdres- gefaald", ex);
+                } finally {
+                    connection.Close();
+                }
+            }
         }
 
         public bool BestaatAdres(int adresId) {
-            throw new NotImplementedException();
+            SqlConnection connection = getConnection();
+            string query = "SELECT count(*) FROM Adres WHERE adresId=@adresId";
+            using (SqlCommand command = connection.CreateCommand()) {
+                connection.Open();
+                try {
+                    command.Parameters.Add(new SqlParameter("@adresId", SqlDbType.Int));
+
+                    command.Parameters["@adresId"].Value = adresId;
+
+                    command.CommandText = query;
+                    int n = (int)command.ExecuteScalar();
+                    if (n > 0) return true; else return false;
+                } catch (Exception ex) {
+                    throw new AdresRepositoryADOException("BestaatAdres(int-id)- gefaald", ex);
+                } finally {
+                    connection.Close();
+                }
+            }
         }
 
         public void UpdateAdres(Adres adres) {
