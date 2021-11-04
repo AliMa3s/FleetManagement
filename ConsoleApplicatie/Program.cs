d00ec00602c5838b0cf1a47632f52b88e0cd320a
yilmaz020 <ahmet.yilmaz@student.hogent.be>
2021-11-04 10:41:39 +0100

    console applicatie aangemaakt

---
+++ ConsoleApplicatie/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApplicatie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string connectionstring = @"mijndatabankconnectionstring";
+            
+        }
+    }
+}
