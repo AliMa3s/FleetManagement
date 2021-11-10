b2f01c4ad9d9f61b40d327c894b7c070b5cd49ba
Filip Rigoir <filip.rigoir@telenet.be>
2021-11-10 14:39:44 +0100

    UnitOfManager verschijven naar WPF

--- ConsoleApplicatie/Program.cs
+++ ConsoleApplicatie/Program.cs
@@ -13,29 +13,10 @@ namespace ConsoleApplicatie
 
             string connectionstring = @"Data Source=.\SQLEXPRESS;Initial Catalog=fleetManagement;Integrated Security=True";
              
-            IUnitOfRepository repos = new UnitOfRepository(connectionstring);  //Maak ADO met alle repos
-            IUnitOfManager managers = new UnitOfManager(repos);  //Maak Managers (erft ook over van IUnitOfRepository)
-
-
-            //Indien gelezen mag dat allemaal weg: is om te communiceren met Ali & Ahmet
-            /************************************/
-            //Idee van Ali was alles tesamen 
-            //Idee van Filip was met Bouwers 
-            //Is alle twee geworden onder één beheer (maar toch staat alles apart voor onderhoud)
-            //Dus in managers zitten alle managers & bouwers (is toegevoegd aan de pattern om in regel te zijn)
-            //** Voertuig & VoertuigBouwer is al gedaan, meer werk hieraan is er niet. Is klaar voor formulier te maken in WPF, te checken en te posten **
-            //(behalve extra interfaces (in het rood) & testen zijn nog niet gedaan)
-
-            /* Voorbeelden:
-            * managers.Voertuigen.BestaatVoertuig();
-            * managers.VoertuigBouwer; 
-            * managers.Bestuurders...
-            * Etc...
-            * 
-            * Een voorbeeld zal duidelijk worden hoe eenvoudig het wel is. 
-            * Je moet maar een punt zetten en je kan de props & methods kiezen uit de lijst
-            */
-            /**************************************/
+            //UnitOfManager & UnitOfRepository is idd voor WPF 
+            //Zullen we later zien als WPF is aangemaakt
+            //Hier komt idd VoertuigRepositoryADO, en de rest ADO 
+            //Deze tekst mag dan weg
 
         }
     }
