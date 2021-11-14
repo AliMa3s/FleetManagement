580567ce2ac49eb2110127a64dc8ed483c4bff39
alimaes <ali.maes@student.hogent.be>
2021-11-14 15:45:09 +0100

    bestuurder voertuigid + foreignkey naar voertuig, voertuig bestuurderid weg

--- FleetManagement.ADO/SQL/Voertuig.sql
+++ FleetManagement.ADO/SQL/Voertuig.sql
@@ -1,19 +1,16 @@
 ﻿CREATE TABLE [dbo].[Voertuig] (
-    [voertuigid]      INT           IDENTITY (1, 1) NOT NULL,
+    [voertuigid]      INT           NOT NULL,
     [automodelid]     INT           NOT NULL,
     [brandstoftypeid] INT           NOT NULL,
-    [kleurnaam]       NVARCHAR(55)  NULL,
-    [bestuurderid]    INT           NULL,
-    [aantal_deuren]   NVARCHAR (15)  NULL,
-    [chassisnummer]   NVARCHAR (20) NOT NULL,
-    [nummerplaat]     NVARCHAR (15)  NOT NULL,
-    [hybride]         tinyint  (2)  NOT NULL,
-    [inboekdatum]     timestamp    NOT NULL,
-    
+    [kleurid]         INT           NULL,
+    [aantal_deuren]   NVARCHAR (1)  NULL,
+    [chassisnummer]   NVARCHAR (17) NOT NULL,
+    [nummerplaat]     NVARCHAR (9)  NOT NULL,
+    [inboekdatum]     ROWVERSION    NOT NULL,
     PRIMARY KEY CLUSTERED ([voertuigid] ASC),
     UNIQUE NONCLUSTERED ([nummerplaat] ASC),
     UNIQUE NONCLUSTERED ([chassisnummer] ASC),
     CONSTRAINT [FK_Voertuig_automodel] FOREIGN KEY ([automodelid]) REFERENCES [dbo].[Automodel] ([automodelid]),
     CONSTRAINT [FK_Voertuig_Brandstoftype] FOREIGN KEY ([brandstoftypeid]) REFERENCES [dbo].[Brandstoftype] ([brandstofid]),
-    CONSTRAINT [FK_Voertuig_Bestuurder] FOREIGN KEY ([bestuurderid]) REFERENCES [dbo].[Bestuurder] ([bestuurderid])
+    CONSTRAINT [FK_Voertuig_kleur] FOREIGN KEY ([kleurid]) REFERENCES [dbo].[Kleur] ([kleurid]),
 );
