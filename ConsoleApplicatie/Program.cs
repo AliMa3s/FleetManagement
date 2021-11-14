10290436aaaf4f93fd73e5581a34f678c01a2022
alimaes <ali.maes@student.hogent.be>
2021-11-14 17:01:27 +0100

    verwijderbestuurder

--- ConsoleApplicatie/Program.cs
+++ ConsoleApplicatie/Program.cs
@@ -14,12 +14,14 @@ namespace ConsoleApplicatie
 
             string connectionstring = @"Data Source=.\SQLEXPRESS;Initial Catalog=fleetManagement;Integrated Security=True";
             BestuurderRepositoryADO bsd = new BestuurderRepositoryADO(connectionstring);
-            Bestuurder b = new Bestuurder(1, "Filip", "Rigoir", "1976-03-31", "B", "1514081390", "76033101986");
-            //bsd.VoegBestuurderToe(b);
+            Bestuurder b = new Bestuurder(4, "Ahmeti", "Yilmaiz", "1976-03-10", "B", "1514081390", "76031010956");//teammeeting bespreken
+            bsd.VoegBestuurderToe(b);
             Console.WriteLine("Bestuurder teogevoegd!");//done 
             Bestuurder bu = new Bestuurder(3, "Filip", "Updated", "1976-03-31", "B", "1514081390", "76033101986");
-            bsd.UpdateBestuurder(bu);
+            //bsd.UpdateBestuurder(bu);
             Console.WriteLine("Bestuurder Updated!");//done 
+            bsd.VerwijderBestuurder(b); //done
+
         }
     }
 }
