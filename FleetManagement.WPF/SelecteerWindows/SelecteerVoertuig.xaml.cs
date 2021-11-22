3625171d35a3f2170c0a83a75a07817bc38091ae
alimaes <ali.maes@student.hogent.be>
2021-11-22 04:53:00 +0100

    SelecteerVoertuigUI(only)-Window

---
+++ FleetManagement.WPF/SelecteerWindows/SelecteerVoertuig.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FleetManagement.WPF.SelecteerWindows {
+    /// <summary>
+    /// Interaction logic for SelecteerVoertuig.xaml
+    /// </summary>
+    public partial class SelecteerVoertuig : Window {
+        public SelecteerVoertuig() {
+            InitializeComponent();
+        }
+    }
+}
