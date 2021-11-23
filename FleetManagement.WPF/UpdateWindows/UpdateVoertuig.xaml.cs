524fe0ae2cb3c213950c5336f3750b64ccd106bd
yilmaz020 <ahmet.yilmaz@student.hogent.be>
2021-11-23 16:42:02 +0100

    voertuig update UI aangemaakt backend moet er nog in

---
+++ FleetManagement.WPF/UpdateWindows/UpdateVoertuig.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FleetManagement.WPF.UpdateWindows
+{
+    /// <summary>
+    /// Interaction logic for UpdateVoertuig.xaml
+    /// </summary>
+    public partial class UpdateVoertuig : Window
+    {
+        public UpdateVoertuig()
+        {
+            InitializeComponent();
+        }
+    }
+}
