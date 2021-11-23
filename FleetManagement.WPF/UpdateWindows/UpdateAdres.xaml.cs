cbb29e0fef5a8f87b70ff3baa41f65e8615f9369
alimaes <ali.maes@student.hogent.be>
2021-11-23 05:51:49 +0100

    UpdateAdresGemaakt

---
+++ FleetManagement.WPF/UpdateWindows/UpdateAdres.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FleetManagement.WPF.UpdateWindows {
+    /// <summary>
+    /// Interaction logic for UpdateAdres.xaml
+    /// </summary>
+    public partial class UpdateAdres : Window {
+        public UpdateAdres() {
+            InitializeComponent();
+        }
+        private void UpdateButton_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void SluitForm_Click(object sender, RoutedEventArgs e) {
+            Window.GetWindow(this).Close();
+        }
+    }
+}
