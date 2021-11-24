667150c63a08bdfe8ad2e9cf93d6adff8b6b2eda
Filip Rigoir <filip.rigoir@telenet.be>
2021-11-24 17:24:34 +0100

    Detailpagina & zoeken gekoppeld

--- FleetManagement.WPF/UserControls/Zoeken/TankkaartZoeken.xaml.cs
+++ FleetManagement.WPF/UserControls/Zoeken/TankkaartZoeken.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using FleetManagement.Manager;
+using FleetManagement.Model;
+using FleetManagement.WPF.DetailWindows;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,18 +25,43 @@ namespace FleetManagement.WPF.UserControls.Zoeken
     {
         private readonly Managers _managers;
 
+        private TankKaart _tankkaart;
+
+        public TankKaart TankkaartWeergave
+        {
+            get => _tankkaart;
+            set
+            {
+                _tankkaart = value;
+                ZoekweergaveTankkaart.SelectedItem = value;
+            }
+        }
+        
+
         public TankkaartZoeken(Managers managers)
         {
             InitializeComponent();
             _managers = managers;
+
+            ZoekweergaveTankkaart.ItemsSource = _managers.TankkaartManager.GeefAlleTankkaart();
         }
 
         private void ZoekWeergave_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            TankkaartWeergave = ZoekweergaveTankkaart.SelectedItem as TankKaart;
         }
 
         private void ZoekenMouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
+            if (_tankkaart != null)
+            {
+                TankkaartDetails detailWindow = new TankkaartDetails(_managers, _tankkaart)
+                {
+                    Owner = Window.GetWindow(this),
+                };
+
+                detailWindow.Show();
+            }
         }
 
         private void SluitVoertuigForm_Click(object sender, RoutedEventArgs e)
@@ -55,6 +82,24 @@ namespace FleetManagement.WPF.UserControls.Zoeken
         private void TextBoxFilterTextChanged(object sender, TextChangedEventArgs e)
         {
 
+        }
+
+        private void KiesDetail_Click(object sender, RoutedEventArgs e)
+        {
+            if (_tankkaart != null)
+            {
+                TankkaartDetails detailWindow = new TankkaartDetails(_managers, _tankkaart)
+                {
+                    Owner = Window.GetWindow(this),
+                };
+
+                detailWindow.Show();
+            }
+        }
+
+        private void SluitVoertuigForm_Click_1(object sender, RoutedEventArgs e)
+        {
+            Window.GetWindow(this).Close();
         }
     }
 }
